f2c7c84ef8cdfbd4a7777aa34d89bacbb3355285
Martin Neumann <martin.neumann@cancom.at>
2025-05-06 17:38:59 +0200

    Auflösung angepasst

--- convert-menu.sh
+++ convert-menu.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-convert -density 600 $1 -scale 1920 -background White -alpha remove -alpha off menu.png
+convert -density 600 "$1" -scale 3140 -background White -alpha remove -alpha off menu.png
